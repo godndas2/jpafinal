d7ed5ffeb1d915a72cf70b3537750042da80fbb2
halfdev <gusgodnd@naver.com>
2019-12-31 00:47:11 +0900

    ref #1 h2 settings

---
+++ src/main/java/org/jpa/study/JpafinalApplication.java
@@ -0,0 +1,13 @@
+package org.jpa.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpafinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpafinalApplication.class, args);
+    }
+
+}
