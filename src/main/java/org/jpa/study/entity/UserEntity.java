e953c8c6bb522cfce97667851955d8f7d0b33194
halfdev <gusgodnd@naver.com>
2020-01-06 18:20:26 +0900

    ref #5 signin test code

--- src/main/java/org/jpa/study/entity/UserEntity.java
+++ src/main/java/org/jpa/study/entity/UserEntity.java
@@ -25,7 +25,7 @@ public class UserEntity {
     private LocalDateTime modifyDT;
 
     @ManyToOne
-    @JoinColumn(name = "team_id", insertable = false, updatable = false)
+    @JoinColumn(name = "team_id")
     private TeamEntity team;
 
     @OneToOne
--- src/test/java/org/jpa/study/entity/UserRepositoryTests.java
+++ src/test/java/org/jpa/study/entity/UserRepositoryTests.java
@@ -1,14 +1,17 @@
 package org.jpa.study.entity;
 
 import org.jpa.study.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -17,15 +20,36 @@ public class UserRepositoryTests {
     @Autowired
     private UserRepository userRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @AfterEach
+    public void cleanup() {
+        userRepository.deleteAll();
+    }
+
     @Test
     public void 회원가입() throws Exception {
         UserEntity userEntity = new UserEntity();
         userEntity.setUserName("tester");
-        userRepository.save(userEntity);
+        entityManager.persist(userEntity);
+//        userRepository.save(userEntity);
+    }
 
-        List<UserEntity> userEntities = userRepository.findAll();
-        userEntities.get(0).getTeam();
+    @Test
+    public void 로그인() throws Exception {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("tester");
+        entityManager.persist(userEntity);
+
+        List<UserEntity> signinUser = userRepository.findAll();
+
+        userEntity = signinUser.get(0);
+
+        assertThat(userEntity.getUserName()).isEqualTo("tester");
+        assertThat(userEntity.getId()).isEqualTo(1);
 
     }
+
 
 }
